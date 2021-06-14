35b21bbfc27c9330aec1fa689d4e6842c1f73f9c
Adrià Alós <adriaalos@gmail.com>
2021-06-14 09:43:53 +0200

    Zones add to MA data. "People consulted" add to assessment.

---
+++ components/assessment-people.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul class="elinor__people ui-rounded-border">
+        <li class="elinor__people-header">
+            {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.title' ) }}
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ assessment[ 'people-manager' ] }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.manager' ) }} 
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ assessment[ 'people-personnel' ] }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.ma-personnel' ) }}
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ assessment[ 'people-government-personnel' ] }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.government-personnel' ) }}
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ assessment[ 'people-community-members' ] }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.local-members' ) }}
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ assessment[ 'people-community-leaders' ] }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.leaders' ) }}
+        </li>
+        <li class="elinor__people-data">
+            <span>{{ people }}</span> {{ $t( 'pages.assessments.content.assessment.tabs.info.data.people-consulted.labels.total' ) }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'assessment-people',
+    computed: {
+        assessment() {
+            return this.$store.state.assessments.assessment
+        },
+        people() {
+            return this.assessment[ 'people-manager' ] + this.assessment[ 'people-personnel' ] + this.assessment[ 'people-government-personnel' ] + this.assessment[ 'people-community-members' ] + this.assessment[ 'people-community-leaders' ]
+        }
+    }
+}
+</script>
--- pages/assessments/_id/info.vue
+++ pages/assessments/_id/info.vue
@@ -1,70 +1,9 @@
 <template>
     <section class="section section--assessment-info">
-        <section class="section section--assessment-data section--mt-0">
-            <div class="container">
-                <header class="header--ma-data">
-                    <h3>{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.header.title' ) }}</h3>
-                </header>
-                <ul class="elinor__data-grid elinor__data-grid--3-5">
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.name' ) }}</span>
-                        <span class="data">{{ ma["name"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.pa-ca-name' ) }}</span>
-                        <span class="data">{{ ma["pa-ca-name"] }}</span>
-                    </li>
-                    <li class="elinor__data">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.establishment' ) }}</span>
-                        <span class="data">{{ ma["establishment"] | formatDate }}</span>
-                    </li>
-                    <li class="elinor__data">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.size' ) }}</span>
-                        <span class="data">{{ ma["size"] }} h</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.pa-ca-name-focal' ) }}</span>
-                        <span class="data">{{ ma["pa-ca-name-focal"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.management-unit' ) }}</span>
-                        <span class="data">{{ ma["manage-unit"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.management-unit-definition' ) }}</span>
-                        <span class="data">{{ ma["manage-unit-definition"] }}</span>
-                    </li>
-                    <li class="elinor__data">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.governance-type' ) }}</span>
-                        <span class="data">{{ ma["governance-type"] }}</span>
-                    </li>
-                    <li class="elinor__data">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.authority-name' ) }}</span>
-                        <span class="data">{{ ma["management-authority"] }}</span>
-                    </li>
-                </ul>
-                <div class="elinor__data-separator"></div>
-                <ul class="elinor__data-grid elinor__data-grid--3-5">
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.rights' ) }}</span>
-                        <span class="data">{{ ma["rights"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.stakeholder-groups' ) }}</span>
-                        <span class="data">{{ ma["stakeholder-groups"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.support' ) }}</span>
-                        <span class="data">{{ ma["support"] }}</span>
-                    </li>
-                    <li class="elinor__data elinor__data--full">
-                        <span class="title">{{ $t( 'pages.managed-areas.content.ma.tabs.info.data.labels.wdpa-id' ) }}</span>
-                        <span class="data">{{ ma["wdpa-id"] }}</span>
-                    </li>
-                </ul>
-            </div>
-        </section>
+
+        <ma-data :ma="ma" :mode="mode"></ma-data>
         <ma-map :ma="ma"></ma-map>
+        <ma-zones :ma="ma"></ma-zones>
     </section>
 </template>
 
@@ -76,19 +15,17 @@ import { mapActions } from 'vuex'
 export default {
     name: 'Assessment',
     layout: 'assessment',
+    data() {
+        return {
+            mode: 'assessment'
+        }  
+    },
     computed: {
         assessment() {
             return this.$store.state.assessments.assessment
         },
         ma() {
             return this.$store.state.ma.mas
-        }
-    },
-    filters: {
-        formatDate: function( value ) {
-            if (value) {
-                return moment(value *1000 ).format('MM/DD/YYYY')
-            }
         }
     }
 }
