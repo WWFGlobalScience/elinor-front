59ef110a1e4ffbb3d2d5b77a21a032ee71548783
Jordi Morera <jmorera@omatech.com>
2022-07-12 08:54:19 +0200

    Update lang file

--- components/documentation/btn-more.vue
+++ components/documentation/btn-more.vue
@@ -1,7 +1,7 @@
 <template>
     <a href="/assessments/edit/35/the-survey" class="btn btn--opacity--child">
-        <span class="btn--opacity__target">Next step</span> 
-        <img src="~/assets/img/ico-select-white.svg" alt="More documents">
+        <span class="btn--opacity__target">{{ $t( 'pages.documentation.body.cards.button' ) }}</span> 
+        <img src="~/assets/img/ico-select-white.svg" alt="">
     </a>
 </template>
     
