51b9c55d1743fabf4429448b3a01a69b1f784f55
Jordi Morera <jmorera@omatech.com>
2022-07-13 17:50:53 +0200

    Add collaboration page

---
+++ pages/collaboration/index.vue
@@ -0,0 +1,98 @@
+<template>
+    <article class="page page--flushed">
+        <section class="section section--main">
+            <img src="~/assets/img/collaboration-section.png" alt="" />
+        </section>
+        <section class="section section--collaboration">
+            <div class="container">
+                <h1 class="c-title--main">{{ $t( 'pages.collaboration.title' ) }}</h1>
+                <div class="g-grid--3-1-lg mt-16">
+                    <div class="g-colspan--2-full-lg">
+                        <div class="text-group">
+                            <h2 class="c-title--md mb-12">Collaboration Principles to guide the use of the Elinor tool and data system </h2>
+                            <p>The Elinor tool and data system is a collaborativeCollaboration is at the heart of learning and working more efficiently and we want to foster a collaborative environment between users of Elinor, and to ensure that users feel comfortable and supported to do so. We value the contributions made by users who have made public their assessments on elinor.org and the Elinor development team in providing the service. Therefore the objective of laying out the principles below is to facilitate collaboration in a way that respects the intellectual property of all those who have contributed to xxxx@elinor.org.</p> 
+                            <p>The principles of openness, respect, transparency, sharing good practice and data accuracy arekey to effective and inclusive collaboration and we hope the guiding principles of collaboration laid out below reflect this and foster fruitful collaborations!  If you would like to make suggestionstowards strengthening collaborations, we welcome them here @ xxxx.elinor.org>. </p>
+                        </div>
+                    </div>
+                    <div class="g-colspan--1-full-lg lg:mt-4">
+                        <blockquote class="elinor__blockquote">
+                            <p>The principles of openness, respect, transparency, sharing good practice and data accuracy arekey to effective and inclusive collaboration and we hope the guiding principles of collaboration laid out below reflect this and foster fruitful collaborations! </p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section section--guidelines">
+            <div class="container">
+                <h2 class="c-title--md mb-12">Guiding principles of collaboration</h2>
+
+                <div class="g-grid--4-1-lg mt-16">
+                    <div class="g-colspan--3-full-lg lg:mt-4">
+                        <ul class="number-list">
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+
+                            <li class="number-list__item">
+                                <div class="number-list__question">
+                                    <h4 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.</h4>
+                                </div>
+                                <div class="number-list__answer">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum magnam labore aliquid quis eveniet! Aliquam porro necessitatibus accusamus magnam nam labore quasi qui, molestiae voluptatem ratione sed quod, in consectetur.
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </article>
+</template>
+<script>
+    export default {
+      name: "collaboration",
+      auth: false
+    };
+</script>
