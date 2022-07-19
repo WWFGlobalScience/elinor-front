299e2a08e62777e8b4baf63cdfbee53388fa3760
Javier Mogollón <jmogollon@omatech.com>
2022-07-20 00:50:52 +0200

    contact with recaptcha

---
+++ store/contact.js
@@ -0,0 +1,34 @@
+export const state = () => ({
+    sent: false,
+    errors: {}
+})
+
+export const mutations = {
+    setSent(state, payload) {
+        state.sent = payload
+    },
+    setErrors(state, payload) {
+        state.errors = payload;
+    }
+}
+
+export const actions = {
+    async contact(state, {name, subject, email, message, recaptcha}) {
+        this.dispatch('loader/loaderState', {
+            active: true,
+            text: 'Sending contact...'
+        })
+
+        try {
+            let url = 'v2/contactelinoradmins';
+            const data = {name, subject, email, message, recaptcha};
+            const response = await this.$axios({method: 'post', url, data});
+            state.commit('setSent', true);
+        } catch (error) {
+            state.commit('setSent', false);
+            state.commit('setErrors', error.response.data);
+        } finally {
+            this.dispatch('loader/loaderState', {active: false});
+        }
+    }
+}
