9a3460bb640b95a14e29143b94e9fbba59eaa71b
Javier Mogollón <jmogollon@omatech.com>
2022-11-11 18:30:32 +0100

    disable download assessment data

--- components/assessment/assessment-actions.vue
+++ components/assessment/assessment-actions.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="assessment__btns">
-        <a v-if="assessment.status === 10" @click="download" role="button" class="btn btn--border-turqy btn--sm" title="Download Data"><img src='~/assets/img/ico-download.svg' alt="Download Data"/> <span>{{ $t('default.downloadData') }}</span></a>
+        <a v-if="false && assessment.status === 10" @click="download" role="button" class="btn btn--border-turqy btn--sm" title="Download Data"><img src='~/assets/img/ico-download.svg' alt="Download Data"/> <span>{{ $t('default.downloadData') }}</span></a>
         <template v-if="$auth.loggedIn">
             <a v-if="!isCreator()" @click="contact" role="button" class="btn btn--border-turqy btn--sm" title="Contact"><img src='~/assets/img/ico-mail.svg' alt="Contact"/> <span>{{ $t('default.contactAdministrator') }}</span></a>
             <a v-if="!isCreator()" @click="flag" role="button" class="btn btn--rounded" title="Flag"><img src="~/assets/img/ico-flag.svg" alt="Flag"> <span class="visually-hidden">Flag</span></a>
