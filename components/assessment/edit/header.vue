5f6f498ef0d90a049fccffe1a914383ce2910cf1
Jordi Morera <jmorera@omatech.com>
2023-05-17 17:02:36 +0200

    Add badge header

--- components/assessment/edit/header.vue
+++ components/assessment/edit/header.vue
@@ -1,23 +1,53 @@
 <template>
     <header class="header--page">
-        <div class="container flex justify-between items-start">
-            <div class="text">
+        <div class="container flex justify-start gap-8 items-start">
+            <div class="flex gap-2 flex-col items-center">
+                <div
+                    class="flex justify-center items-center w-[80px] h-[80px] rounded-full bg-excellent"
+                >
+                    <span class="text-white text-[40px] font-semibold">99</span>
+                </div>
+                <span class="uppercase text-grayy-lighter font-bold text-[12px]"
+                    >out of 100</span
+                >
+            </div>
+            <div class="text flex-1">
                 <h1 class="mb-0">
-                    <img src="~/assets/img/ico-assessments-turqy.svg">
-                    <span>{{ $t( 'pages.assessments.edit.title' ) }}</span>
+                    <img src="~/assets/img/ico-assessments-turqy.svg" />
+                    <span>{{ $t("pages.assessments.edit.title") }}</span>
                 </h1>
                 <h2 class="assessment__name">{{ assessment.name }}</h2>
-                <p class="assessment__year">{{ $t('pages.assessments.edit.year') }} {{ assessment.year }}</p>
+                <p class="assessment__year">
+                    {{ $t("pages.assessments.edit.year") }}
+                    {{ assessment.year }}
+                </p>
                 <assessment-actions type="edit" />
             </div>
             <div class="header__save" style="justify-items: flex-start">
                 <assessment-status class="mb-7"></assessment-status>
                 <template v-if="assessment.status !== 10">
-                    <button type="submit" form="form--assessment-edit" class="btn btn--opacity--child">
-                        <span class="btn--opacity__target">{{ $t('pages.assessments.edit.saveButton') }}</span>
-                        <img src="~/assets/img/ico-save-white.svg">
+                    <button
+                        type="submit"
+                        form="form--assessment-edit"
+                        class="btn btn--opacity--child"
+                    >
+                        <span class="btn--opacity__target">{{
+                            $t("pages.assessments.edit.saveButton")
+                        }}</span>
+                        <img src="~/assets/img/ico-save-white.svg" />
                     </button>
-                    <p v-if="assessment.last_edit !== null && assessment.last_edit !== undefined"> {{ $t( 'default.save.autosave' ) + ' ' + assessment.last_edit.fromNow() }}</p>
+                    <p
+                        v-if="
+                            assessment.last_edit !== null &&
+                                assessment.last_edit !== undefined
+                        "
+                    >
+                        {{
+                            $t("default.save.autosave") +
+                                " " +
+                                assessment.last_edit.fromNow()
+                        }}
+                    </p>
                 </template>
             </div>
         </div>
@@ -25,14 +55,14 @@
 </template>
 
 <script>
-import {mapState} from "vuex";
+import { mapState } from "vuex";
 
 export default {
-    name: 'assessment-edit-header',
+    name: "assessment-edit-header",
     computed: {
         ...mapState({
             assessment: state => state.assessments.assessment
         })
-    },
-}
+    }
+};
 </script>
--- components/assessment/header.vue
+++ components/assessment/header.vue
@@ -3,12 +3,16 @@
         <div class="container flex justify-between items-start">
             <div class="text">
                 <h1 class="mb-0">
-                    <img src="~/assets/img/ico-assessments-turqy.svg">
-                    <span>{{ $t('pages.assessments.view.title') }}</span>
+                    <img src="~/assets/img/ico-assessments-turqy.svg" />
+                    <span>{{ $t("pages.assessments.view.title") }}</span>
                 </h1>
                 <h2 class="assessment__name">{{ assessment.name }}</h2>
-                <p class="assessment__year">{{ $t('pages.assessments.view.year') }} {{ assessment.year }} - Version {{ assessment.published_version }}</p>
-                <assessment-actions type="show"/>
+                <p class="assessment__year">
+                    {{ $t("pages.assessments.view.year") }}
+                    {{ assessment.year }} - Version
+                    {{ assessment.published_version }}
+                </p>
+                <assessment-actions type="show" />
             </div>
             <div class="header__save" style="justify-content: flex-start">
                 <assessment-status></assessment-status>
@@ -19,11 +23,11 @@
 
 <script>
 export default {
-    name: 'assessment-header',
+    name: "assessment-header",
     computed: {
         assessment() {
-            return this.$store.state.assessments.assessment
+            return this.$store.state.assessments.assessment;
         }
     }
-}
+};
 </script>
