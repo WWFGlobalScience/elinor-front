a5d87d9c5419ab4527330a45ac09126c6adcd4b4
Catalyze <analytics@catalyzecommunications.com>
2025-01-27 22:26:14 +0700

    Fix hide profile and lang-switcher dropdown when click outside

--- store/dropdown.js
+++ store/dropdown.js
@@ -6,10 +6,16 @@ export const mutations = {
     toggleDropdown(state) {
         state.dropdown = !state.dropdown;
     },
+    closeDropdown(state) {
+        state.dropdown = true;
+    }
 };
 
 export const actions = {
     toggleDropdown(state) {
         state.commit('toggleDropdown');
     },
+    closeDropdown(state) {
+        state.commit('closeDropdown');
+    }
 };
