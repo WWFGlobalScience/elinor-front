c14c6c40dcc064dac6300c0378e0b652ff9bd434
Nasser Olwero <jgnasser@gmail.com>
2025-02-08 12:41:42 -0500

    edit how to use section on home page and test cache

--- components/home/public.vue
+++ components/home/public.vue
@@ -285,7 +285,7 @@
                         How to use this tool
                     </h1>
                     <p>
-                        See below how to use Elinor
+                        See below how Elinor works and hot to use it
                     </p>
                 </div>
             </div>
