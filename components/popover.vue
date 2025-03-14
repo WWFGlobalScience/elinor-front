4f1355cbd27cba58370b37fe242d5cddb217901c
Nasser Olwero <jgnasser@gmail.com>
2025-03-13 21:22:56 -0400

    Testing browser cache in main

--- components/popover.vue
+++ components/popover.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="popover">
-        Help improve Elinor!! Click <a href="https://docs.google.com/forms/d/e/1FAIpQLSccumYV8Ldg4XJZDmc7FIR1vaSRXXkRfWnrxVPa4T3dqW_cdw/viewform" target="_blank" rel="noopener noreferrer">here</a> to complete a 5-10 minute survey on your experience using <a href="https://ElinorData.org">ElinorData.org</a>
+        Help improve Elinor! Click <a href="https://docs.google.com/forms/d/e/1FAIpQLSccumYV8Ldg4XJZDmc7FIR1vaSRXXkRfWnrxVPa4T3dqW_cdw/viewform" target="_blank" rel="noopener noreferrer">here</a> to complete a 5-10 minute survey on your experience using <a href="https://ElinorData.org">ElinorData.org</a>
     </div>
 </template>
 
